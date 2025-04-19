048a6f4435d16c4c3fdadca954e05acf81943d58
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-20 01:43:48 +0530

    run method

--- MyThread.java
+++ MyThread.java
@@ -3,13 +3,4 @@
 public class MyThread extends Thread {
     // This method contains the logic to be executed in a separate thread
     public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Thread from MyThread: " + i);
-            try {
-                Thread.sleep(500); // Sleep to simulate delay
-            } catch (InterruptedException e) {
-                System.out.println("Thread interrupted");
-            }
-        }
-    }
-}
+        
