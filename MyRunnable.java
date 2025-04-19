7c94976f8a23745260d83485b0236cf9fea7b8ec
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-20 01:35:07 +0530

    creating class runnable

--- MyRunnable.java
+++ MyRunnable.java
@@ -1,15 +1,4 @@
 //MyRunnable.java
 
 public class MyRunnable implements Runnable {
-    // This method contains the logic to be executed in a thread
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Thread from MyRunnable: " + i);
-            try {
-                Thread.sleep(500); // Simulate processing time
-            } catch (InterruptedException e) {
-                System.out.println("Runnable thread interrupted");
-            }
-        }
-    }
-}
+   
