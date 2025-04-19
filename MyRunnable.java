52813dc98a0a83ed006377634d8c6dd2f6026c77
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-20 01:35:45 +0530

    for loop

--- MyRunnable.java
+++ MyRunnable.java
@@ -3,4 +3,5 @@
 public class MyRunnable implements Runnable {
     // This method contains the logic to be executed in a thread
     public void run() {
-       
+        for (int i = 1; i <= 5; i++) {
+            
