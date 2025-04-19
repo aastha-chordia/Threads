2b3a7746722f9c52ea6053171a4ec69045654c38
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-20 01:33:25 +0530

    Create Main class

--- Main.java
+++ Main.java
@@ -3,11 +3,4 @@
 // Batch: AIML A1
 
 public class Main {
-    public static void main(String[] args) {
-        // Starting threads using separate operations
-        ThreadOperations operations = new ThreadOperations();
-
-        operations.runThreadClass();
-        operations.runRunnableClass();
-    }
-}
+ 
