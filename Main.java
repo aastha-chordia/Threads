b7e0c653e013b14dddec9534d3e144b409e16008
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-20 01:34:19 +0530

    operations

--- Main.java
+++ Main.java
@@ -6,3 +6,8 @@ public class Main {
     public static void main(String[] args) {
         // Starting threads using separate operations
         ThreadOperations operations = new ThreadOperations();
+        
+        operations.runThreadClass();
+        operations.runRunnableClass();
+    }
+}
