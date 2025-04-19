5d9e30b6e42b40095c260e277af19e54c735a742
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-20 01:49:28 +0530

    content

--- ThreadOperations.java
+++ ThreadOperations.java
@@ -13,4 +13,7 @@ public class ThreadOperations {
         }
     }
     public void runRunnableClass() {
-        
+        System.out.println("\nRunning Thread using MyRunnable class:");
+        Thread t2 = new Thread(new MyRunnable());
+        t2.start();
+       
