4353be26421df8f243843dace4b6b430a9b3de64
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-20 01:47:35 +0530

    creating and starting new thread

--- ThreadOperations.java
+++ ThreadOperations.java
@@ -3,4 +3,7 @@
 public class ThreadOperations {
     
     public void runThreadClass() {
-        
+        System.out.println("\nRunning Thread using MyThread class:");
+        MyThread t1 = new MyThread();
+        t1.start();
+       
