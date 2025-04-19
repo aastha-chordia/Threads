c5a089428b758090a4f566faf4e9d22531524d0b
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-20 01:36:09 +0530

    try block (exception)

--- MyRunnable.java
+++ MyRunnable.java
@@ -4,4 +4,7 @@ public class MyRunnable implements Runnable {
     // This method contains the logic to be executed in a thread
     public void run() {
         for (int i = 1; i <= 5; i++) {
-            
+            System.out.println("Thread from MyRunnable: " + i);
+            try {
+                Thread.sleep(500); // Simulate processing time
+            } 
