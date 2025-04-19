e6f31665754d18d043e3d428081bd6b0f8f773a1
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-20 01:44:10 +0530

    for loop

--- MyThread.java
+++ MyThread.java
@@ -3,4 +3,6 @@
 public class MyThread extends Thread {
     // This method contains the logic to be executed in a separate thread
     public void run() {
-        
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread from MyThread: " + i);
+            
